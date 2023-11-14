c74dd25de7491cb7e033fad4dab04c331e28becc
sinead <22346937@studentmail.ul.ie>
2023-11-14 14:30:34 +0000

    update student

---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    private String first_Name;
+    private String last_Name;
+    private int id;
+    private String address;
+    private String email;
+    private int phone;
+    public Student(String first_Name,String last_Name,int id,String address,String email,int phone){
+        //check if email is valid ie must contain and at @ and a .
+        //changing details
+        //get and set
+
+        this.first_Name = first_Name;
+        this.last_Name = last_Name;
+        this.id = id;
+        this.address = address;
+        this.email = email;
+        this.phone = phone;
+        //in order to create a student you need the following details
+
+    }
+    public void setStudent(int id){
+
+    }
+}
