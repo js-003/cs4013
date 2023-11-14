7c60d01c3be50ccb35bb7b971f01bb917f29372d
js-003 <jakubsokal13@gmail.com>
2023-11-14 14:38:44 +0000

    Lecturer Updated

--- main/Lecturer.java
+++ main/Lecturer.java
@@ -4,19 +4,19 @@ public class Lecturer {
     public Lecturer(String firstName,String lastName, String moduleCode, String phoneNumber, String email){
         if(firstName.matches("[A-Z]\\w*")){
             this.firstName = firstName;
-        }else System.out.println("A");
+        }else throw new IllegalArgumentException("Check your first name");
         if(lastName.matches("[A-Z]\\w*")) {
             this.lastName = lastName;
-        }else System.out.println("B");
+        }else throw new IllegalArgumentException("Check your last name");
         if(Character.isLetter(moduleCode.charAt(0))&&Character.isLetter(moduleCode.charAt(1))&& moduleCode.matches("\\w{2}\\d{4}")){
             this.moduleCode = moduleCode;
-        }else System.out.println("C");
+        }else throw new IllegalArgumentException("Incorrect Module Code");
         if(phoneNumber.matches("[+]\\w{2,3}\\w{9,12}")){
             this.phoneNumber = phoneNumber;
-        }else System.out.println("D");
+        }else throw new IllegalArgumentException("Incorrect Phone Number");
         if(email.matches(".*@\\w+.\\w+")) {
             this.email = email;
-        }else System.out.println("e");
+        }else throw new IllegalArgumentException("Incorrect Email");
     }
 
     public static void main(String[] args) {
