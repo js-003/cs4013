022f3942d24e23959b2ac16648ff66738517c855
irene <22299475@studentmail.ul.ie>
2023-11-18 23:11:29 +0000

    Feat: added base skeleton

---
+++ main/Gradebook.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class Gradebook {
+    private HashMap<String, Grades> grades = new HashMap<>();
+
+    public void addGrade(String test, Grades results) {
+
+    }
+
+    public Double getAverage(String ID) {
+        return 0.0;
+    }
+
+    public Double getClassAverage() {
+        return 0.0;
+    }
+}
