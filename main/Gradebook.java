6089d4bdbd723105bcbb41f8bfe6c348361c069e
irene <22299475@studentmail.ul.ie>
2023-11-22 17:46:08 +0000

    Code formatted

--- main/Gradebook.java
+++ main/Gradebook.java
@@ -6,28 +6,29 @@ public class Gradebook {
     public void addGrade(String test, Grades results) {
         this.grades.put(test, results);
     }
+
     public Double getAverage(String test) {
         Integer total = 0;
         Grades results = grades.get(test);
-        for(Integer grade : results.getResults().values()) {
+        for (Integer grade : results.getResults().values()) {
             total += grade;
         }
-        return (double) (total/results.getResults().values().size());
+        return (double) (total / results.getResults().values().size());
     }
 
     public Double getClassAverage() {
         Double total = 0.0;
-        for(String test : this.grades.keySet()) {
+        for (String test : this.grades.keySet()) {
             total += this.getAverage(test);
         }
-        return (total/this.grades.values().size());
+        return (total / this.grades.values().size());
     }
 
     public Double getStudentAverage(String ID) {
         Integer total = 0;
-        for(String test : this.grades.keySet()) {
+        for (String test : this.grades.keySet()) {
             total += this.grades.get(test).getResult(ID);
         }
-        return (double) (total/this.grades.values().size());
+        return (double) (total / this.grades.values().size());
     }
 }
