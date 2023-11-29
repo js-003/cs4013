c9dccd3232fd8296b08f6a7cc139069d4ee9464f
sinead <22346937@studentmail.ul.ie>
2023-11-29 12:03:50 +0000

    made a few changes to semester class

--- main/Semester.java
+++ main/Semester.java
@@ -1,39 +1,39 @@
 
-import java.util.Date;
 import java.util.ArrayList;
 
 
 public class Semester {
     private int semesterNumber;
-    private Date startDate;
-    private Date endDate;
-    private ArrayList<Module> SemesterModules;
+    private ArrayList<Module> SemesterModules; //creates an arraylist of type module
     private ArrayList<Semester> allSemesters;
 
     public Semester(int semesterNumber){
-        //this method only takes into account 4 year courses (8 semesters)
         this.semesterNumber= semesterNumber;
-        this.startDate= startDate;
-        this.endDate= endDate;
+        this.SemesterModules= new ArrayList<Module>();
 
     }
-    public void addModulesToSemester(Semester s,Module m){
-        SemesterModules.add(m);
-        //add the specified module to the specified semester
+    public void addModulesToSemester(Semester s,Module m) {
+        if (s.doesSemesterExist(allSemesters, semesterNumber)) {
+            SemesterModules.add(m);
+            //add the specified module to the specified semester
+        }
     }
     public ArrayList<Module> getSemesterModules(){
         return SemesterModules;
         //returns the modules in the semester
     }
 
-    //adds a semester to an array list of semesters
-    public void addSemester(Semester s) {
-        // Checks if the semester is not already in the allSemesters list
-        if (!allSemesters.contains(s)) {
-            allSemesters.add(s);
-        } else {
-            System.out.println("Semester " + s + " already exists.");
-        }
+    public int getSemesterNumber() {
+        return semesterNumber;
+    }
 
+    //a method to check wether or not a semester exists
+    public static boolean doesSemesterExist(ArrayList<Semester> allSemesters, int semesterNumber) {
+        for (Semester semester : allSemesters) {
+            if (semester.getSemesterNumber() == semesterNumber) {
+                return true;
+            }
+        }
+        return false;
     }
 }
