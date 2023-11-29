67a74612095e8d85c13ec0345a603e0d2c0c1340
eryk-mar <129205520+eryk-mar@users.noreply.github.com>
2023-11-29 00:13:24 +0000

    Update Grades.java

--- main/Grades.java
+++ main/Grades.java
@@ -1,23 +1,35 @@
-import java.util.HashMap;
-
+mport java.util.HashMap;
+import java.util.ArrayList;
 public class Grades {
-    private static int[] percentage = {-1,0,30,35,40,48,52,56,60,64,72,80};
-    private String[] gradeText = {"NG","F","D2","D1","C3","C2","C1","B3","B2","B1","A2","A1"};
-    private HashMap<String, String> results = new HashMap<>();
+    private static int[] percentage = {0,30,35,40,48,52,56,60,64,72,80};
+    private String[] gradeText = {"F","D2","D1","C3","C2","C1","B3","B2","B1","A2","A1"};
+
+    private HashMap<String, ArrayList<Object>> results = new HashMap<>();
+    private double[] gcaScore = {0.00, 1.20, 1.60, 2.00, 2.40, 2.60, 2.80, 3.00, 3.20, 3.60, 4.00};
 
     public Grades(String testName,double result){
-        results.put(testName,gradeConvert(result));
+        ArrayList<Object> info = new ArrayList<>();
+        info.add(gradeConvert(result));
+        info.add(scoreConvert(result));
+        results.put(testName, info);
     }
 
     public String gradeConvert(double result){
-        if(result>-1){
-            for(int i = percentage.length-1; i>-1;i--) {
-                if (result >= percentage[i]) {
-                    return gradeText[i];
-                }
+        for(int i = percentage.length-1; i>-1;i--){
+            if(result>=percentage[i]){
+                return gradeText[i];
             }
         }
-        return "Cannot Have A Negative Percentage";
+        return "NG";
+    }
+
+    public double scoreConvert(double result){
+        for(int i = percentage.length-1; i>-1;i--){
+            if(result>=percentage[i]){
+                return gcaScore[i];
+            }
+        }
+        return 0.00;
     }
 
     public String giveIGrade(){
@@ -28,5 +40,3 @@ public class Grades {
         return results.toString();
     }
 }
-
-
