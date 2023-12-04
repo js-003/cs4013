a46d14bea96c71e9473b1b03c60da94b096c8bff
js-003 <jakubsokal13@gmail.com>
2023-12-04 01:50:23 +0000

    Java docs comments added

--- main/Course.java
+++ main/Course.java
@@ -7,7 +7,7 @@ public class Course {
     private final String courseCode;
     private String Level;
 
-    private final int yearOfStudy;
+    private final int duration;
 
     private ArrayList<Student> studentList;
 
@@ -15,31 +15,66 @@ public class Course {
 
     private ArrayList<Semester> semester;
 
-    public Course(String courseCode, String courseName, int yearOfStudy){
+    /**
+     * Creates a course object by passing the parameters below
+     *
+     * @param courseCode The code of a course
+     * @param courseName The name of a course
+     * @param duration The duration of a course
+     */
+
+    public Course(String courseCode, String courseName, int duration){
         studentList = new ArrayList<>();
         moduleList = new ArrayList<>();
         semester = new ArrayList<>();
         this.courseCode = courseCode;
         this.courseName = courseName;
-        this.yearOfStudy = yearOfStudy;
+        this.duration = duration;
     }
+
+    /**
+     * Changes the name of a course already created
+     *
+     * @param newCourseName The new name for a course
+     */
 
     public void setCourseName(String newCourseName){
         this.courseName = newCourseName;
     }
 
+    /**
+     * Returns the name of a course already created
+     *
+     * @return
+     */
     public String getCourseName(){
         return courseName;
     }
 
+    /**
+     * Adds a student object to a course already created
+     *
+     * @param s The student object getting added to a course
+     */
     public void addStudent(Student s){
         this.studentList.add(s);
     }
 
+    /**
+     * Removes a student object from a course already created
+     *
+     * @param s The student object getting removed from a course
+     */
     public void removeStudent(Student s){
         this.studentList.remove(s);
     }
 
+    /**
+     * Returns a student object to a string if such student exists in a course
+     *
+     * @param s The student object getting searched
+     * @return The details of a student in string format if the student is found
+     */
     public String getStudent(Student s){
         String c = s.getId()+" "+s.getFirstName()+" "+s.getLastName();
         for (Student string : studentList) {
@@ -50,6 +85,12 @@ public class Course {
         return "No Student " + s.toString();
     }
 
+    /**
+     * Gives a course a graduate level
+     *
+     * @param level The level of the course
+     */
+
     public void graduateLevel(String level){
         this.degree = level;
         if(level.toLowerCase().contains("bachelor")){
@@ -59,43 +100,103 @@ public class Course {
         }else this.graduateLevel = "Postgraduate";
     }
 
+    /**
+     * Returns a list of students in a course
+     *
+     * @return A list of students in string format
+     */
     public String getStudentList(){
         return studentList.toString();
     }
 
+    /**
+     * Returns a list of modules in a course
+     *
+     * @return A list of modules in a course
+     */
     public String getModuleList(){
         return moduleList.toString();
     }
 
+    /**
+     * Returns the degree of a course
+     *
+     * @return The degree of a course
+     */
     public String getDegree() {
         return degree;
     }
 
+    /**
+     * Returns the graduate level of a course
+     *
+     * @return The graduate level of a course
+     */
     public String getGraduateLevel() {
         return graduateLevel;
     }
 
+    /**
+     * Returns the course code of a course
+     *
+     * @return The course code of a course
+     */
     public String getCourseCode() {
         return courseCode;
     }
 
-    public int getYearOfStudy() {
-        return yearOfStudy;
+    /**
+     * Returns the duration of a course in years
+     *
+     * @return The duration of a course
+     */
+
+    public int getDuration() {
+        return duration;
     }
 
+    /**
+     * Adds a module object to a course
+     *
+     * @param m The module getting added to a course
+     */
     public void addModule(Module m){
         moduleList.add(m.getModuleName());
     }
 
+    /**
+     * Adds a semester object to a course
+     *
+     * @param s The semester getting added
+     */
     public void addSemester(Semester s){
         semester.add(s);
     }
 
+    /**
+     * Returns the semesters in a course
+     *
+     * @return The semesters in a course
+     */
     public String getSemesters(){
         return semester.toString();
     }
 
-    public String toString(){
+    /**
+     * Returns the details of a course in string format
+     *
+     * @return The details of a course
+     */
+    public String getDetails(){
         return String.format("Course Name: %s\nCourse Code: %s\nSemester: %s\nModule List: %s\nStudent List: %s",getCourseName(),getCourseCode(),getSemesters(),getModuleList(),getStudentList());
+    }
+
+    /**
+     * Returns the details of a course needed for the database
+     *
+     * @return Simpler version of details of a course
+     */
+    public String toString(){
+        return String.format("%s %s %d",getCourseCode(),getCourseName(),getDuration());
     }
 }
--- main/Lecturer.java
+++ main/Lecturer.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
 
+/**
+ * @author Jakub
+ */
 public class Lecturer {
-
     private String firstName;
     private String surname;
     private String moduleCode;
@@ -13,6 +15,16 @@ public class Lecturer {
 
     private String username;
 
+    /**
+     * Create a lecturer object passing in the parameters below
+     *
+     * @param firstName Lecturers first name
+     * @param surname Lecturers surname
+     * @param moduleCode Lecturers module code
+     * @param phoneNumber Lecturers phone number
+     * @param email Lecturers email
+     * @param department Lecturers department
+     */
     public Lecturer(String firstName,String surname, String moduleCode, String phoneNumber, String email, String department){
         this.firstName = firstName;
         this.surname = surname;
@@ -23,62 +35,128 @@ public class Lecturer {
         generateUsername();
     }
 
+    /**
+     * Generates a username for each lecturer created
+     */
     public void generateUsername(){
         this.username = this.firstName.toLowerCase()+"."+this.surname.toLowerCase();
     }
 
-
+    /**
+     * Returns the module code of a lecturer
+     *
+     * @return The module code of a lecturer
+     */
     public String getModuleCode(){
         return this.moduleCode;
     }
+
+    /**
+     * Returns the first name of a lecturer
+     *
+     * @return The first name of a lecturer
+     */
 
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Changes the first name of a lecturer
+     *
+     * @param firstName The new name for a lecturer
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Returns the surname of a lecturer
+     *
+     * @return Surname of a lecturer
+     */
     public String getSurname() {
         return surname;
     }
+
+    /**
+     * Changes the surname of a lecturer
+     *
+     * @param newSurname The new surname for a lecturer
+     */
     public void setSurname(String newSurname) {
         this.surname = surname;
     }
 
+    /**
+     * Returns the phone number of a lecturer
+     *
+     * @return The phone number of a lecturer
+     */
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    /**
+     * Changes the phone number of a lecturer
+     *
+     * @param newNumber The new phone number of a lecturer
+     */
     public void setPhoneNumber(String newNumber) {
         this.phoneNumber = newNumber;
     }
 
+    /**
+     * Returns the email of a lecturer
+     *
+     * @return The email of a lecturers
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Returns the login username of a lecturer
+     *
+     * @return The login username of a lecturers
+     */
     public String getUsername(){
         return this.username;
     }
+
+    /**
+     * Changes the email of a lecturer
+     *
+     * @param newEmail The new email for a lecturer
+     */
     public void setEmail(String newEmail) {
         this.email = newEmail;
     }
 
+    /**
+     * Returns all lecturer details in string format
+     *
+     * @return The string format of a lecturers details
+     */
     public String toString(){
         return String.format("%s,%s,%s,%s,%s,%s",getUsername(),getFirstName(),getSurname(),getModuleCode(),getPhoneNumber(),getEmail());
     }
 
+    /**
+     * Returns the department of a lecturer
+     *
+     * @return The department of a lecturers
+     */
     public String getDepartment() {
         return department;
     }
 
+    /**
+     * Changes the department of a lecturer
+     *
+     * @param department The new department for a lecturer
+     */
     public void setDepartment(String department) {
         this.department = department;
-    }
-
-    public static void main(String[] args) {
-        Lecturer n = new Lecturer("Mi","Eng","CS4013","+353876523466","m.eng@ul.ie","Science");
     }
 }
